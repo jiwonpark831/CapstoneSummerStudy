b46c98a7b08024d9ec104064f75c2935109c8646
jiwonpark831 <jiwonpark831@gmail.com>
2026-07-31 16:21:05 +0900

    [Final] 큰 틀 22

--- FinalProject/p2p.c
+++ FinalProject/p2p.c
@@ -149,27 +149,117 @@ void *sender_send(void *arg)
 
 void receiver(char *argv[])
 {
-    // listen
+    // sender랑 연결
+    int receive_sd;
+    struct sockaddr_in sender_adr;
+
+    // p2p listen으로 연결
+    int receive_p2p_accept_sd, receive_p2p_request_accpet_sd;
+    struct sockaddr_in receive_p2p_accept_sd_adr, receive_p2p_request_accpet_sd_adr;
+    int receive_p2p_request_accpet_sd_adr_sz;
+
+    // p2p connect로 연결
+    int receive_p2p_connect_sd;
+    struct sockaddr_in receive_p2p_sender_adr;
+
+    int write_flag = 0;
+    struct Receiver_info other_receiver_info;
+    int my_index;
+    pthread_t send_thread, receive_thread;
+
+    // p2p connect를 위한 listen
+    receive_p2p_accept_sd = socket(PF_INET, SOCK_STREAM, 0);
+
+    memset(&receive_p2p_accept_sd_adr, 0, sizeof(receive_p2p_accept_sd_adr));
+    receive_p2p_accept_sd_adr.sin_family = AF_INET;
+    receive_p2p_accept_sd_adr.sin_addr.s_addr = htonl(INADDR_ANY);
+    receive_p2p_accept_sd_adr.sin_port = htons(atoi(argv[2]));
+
+    if (bind(receive_p2p_accept_sd, (struct sockaddr *)&receive_p2p_accept_sd_adr, sizeof(receive_p2p_accept_sd_adr)) == -1)
+        error_handling("bind() error");
+    if (listen(receive_p2p_accept_sd, 10) == -1)
+        error_handling("listen() error");
+
     // sender랑 connect
-    // int write_flag
-    // info 받아오기
-    // 자기보다 작은애 accept
-    // ptherad_create(receiver_send)
-    // ptherad_create(receiver_receive)
-    // connect (자기보다 큰애한태)
-    // ptherad_create(receiver_send)
-    // ptherad_create(receiver_receive)
-    // while로 data 읽기, writeflag로 트리거
+    receive_sd = socket(PF_INET, SOCK_STREAM, 0);
+    if (receive_sd == -1)
+        error_handling("socket() error");
+
+    memset(&sender_adr, 0, sizeof(sender_adr));
+    sender_adr.sin_family = AF_INET;
+    sender_adr.sin_addr.s_addr = inet_addr(argv[5]);
+    sender_adr.sin_port = htons(atoi(argv[6]));
+
+    if (connect(receive_sd, (struct sockaddr *)&sender_adr, sizeof(sender_adr)) == -1)
+        error_handling("connect() error");
+    else
+        puts("Connected..................");
+
+    write(receive_sd, argv[2], sizeof(argv[2]));
+
+    // 다른 receiver 정보, seg info 등 받아오기
+    read(receive_sd, &other_receiver_info, sizeof(other_receiver_info));
+
+    // my_index보다 작은 index 값이면 accept
+    for (int i = 0; i < my_index; i++)
+    {
+
+        receive_p2p_request_accpet_sd_adr_sz = sizeof(receive_p2p_request_accpet_sd_adr);
+        receive_p2p_request_accpet_sd = accept(receive_p2p_accept_sd, (struct sockaddr *)&receive_p2p_request_accpet_sd_adr, &receive_p2p_request_accpet_sd_adr_sz);
+
+        pthread_create(&send_thread, NULL, sender_send, (void *)&receive_p2p_request_accpet_sd);
+        pthread_create(&receive_thread, NULL, receiver_receive, (void *)&receive_p2p_request_accpet_sd);
+    }
+
+    // my_index보다 큰 애한테 connect
+    for (int j = my_index; j < seg_count; j++)
+    {
+        receive_p2p_connect_sd = socket(PF_INET, SOCK_STREAM, 0);
+        if (receive_p2p_connect_sd == -1)
+            error_handling("socket() error");
+
+        memset(&receive_p2p_sender_adr, 0, sizeof(receive_p2p_sender_adr));
+        receive_p2p_sender_adr.sin_family = AF_INET;
+        receive_p2p_sender_adr.sin_addr.s_addr = inet_addr(other_receiver_info[j].ip);
+        receive_p2p_sender_adr.sin_port = htons(atoi(other_receiver_info[j].port));
+
+        if (connect(receive_p2p_connect_sd, (struct sockaddr *)&receive_p2p_sender_adr, sizeof(receive_p2p_sender_adr)) == -1)
+            error_handling("connect() error");
+        else
+            puts("Connected..................");
+
+        pthread_create(&send_thread, NULL, sender_send, (void *)&receive_p2p_connect_sd);
+        pthread_create(&receive_thread, NULL, receiver_receive, (void *)&receive_p2p_connect_sd);
+    }
+
+    // sender에게 받은 데이터 read
+    while (1)
+    {
+        read(receive_sd, buf, sizeof(buf));
+        strcat(짱 큰 배열, buf);
+        write_flag++;
+    }
+
     // pthread join
+    // pthread_join();
 }
 
 void *receiver_send(void *arg)
 {
-    // write flag가 바뀌면 write
+    int flag;
+    // if (flag != write_flag)
+    // {
+    //     write()
+    //         flag = write_flag;
+    // }
 }
 
 void *receiver_receive(void *arg)
 {
+    while (1)
+    {
+        // read();
+    }
 }
 
 void opt_setting(char *argv[])
